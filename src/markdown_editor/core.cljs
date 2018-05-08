65632fff1f0737a3d5bf21f1065cb81b1e6791c1
Daw-Ran Liou <dawran6@gmail.com>
2018-05-08 15:21:54 -0700

    Add client-side download button

--- src/markdown_editor/core.cljs
+++ src/markdown_editor/core.cljs
@@ -9,17 +9,40 @@
 (defn update-text [event]
   (swap! app-state assoc :text event.target.value))
 
-(defn hello-world []
+(defn download-markdown [event]
+  (.preventDefault event)
+  (let [data (js/Blob. [(:text @app-state)] {:type "text/plain;charset=utf-8;"})
+        url (.createObjectURL (.-URL js/window) data)
+        tmp-link (.createElement js/document "a")]
+    (set! (.-href tmp-link) url)
+    (set! (.-download tmp-link) "content.md")
+    (.appendChild (.-body js/document) tmp-link)
+    (.click tmp-link)
+    (.removeChild (.-body js/document) tmp-link)))
+
+(defn menu-bar []
+  [:ul
+   [:li]])
+
+(defn editor [app-state]
   [:div
    {:id "editor"}
    [:textarea
     {:onChange update-text}
-    (:text @app-state)]
+    (:text app-state)]
    [:div
-    {:id "preview"
-     :dangerouslySetInnerHTML {:__html (md->html (:text @app-state))}}]])
+    [:div
+     {:id "preview"
+      :dangerouslySetInnerHTML {:__html (md->html (:text app-state))}}]]
+   [:button
+    {:onClick download-markdown}
+    "Download Markdown"]])
 
-(reagent/render-component [hello-world]
+(defn app []
+  [editor @app-state]
+  )
+
+(reagent/render-component [app]
                           (. js/document (getElementById "app")))
 
 (defn on-js-reload []
