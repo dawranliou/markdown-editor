abde9a2650c583df9bc41e20f8a933ac69f05460
Daw-Ran Liou <dawran6@gmail.com>
2018-12-16 15:38:37 -0800

    Move app-state to value prop of textarea

--- src/markdown_editor/core.cljs
+++ src/markdown_editor/core.cljs
@@ -23,8 +23,8 @@
 (defn editor [app-state]
   [:div.w-100.m0.h100.dib.h-100
    [:textarea.fl.w-50.h-100.bg-black-10.br.b--black-10.pa4.f4.code
-    {:onChange update-text}
-    (:text app-state)]
+    {:onChange update-text
+     :value (:text app-state)}]
    [:div.fl.w-50.h-100.ph4
     [:div
      {:id "preview"
