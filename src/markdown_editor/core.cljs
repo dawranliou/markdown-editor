b30126b66d38cebbd22d2c75ddecc33464d52711
Daw-Ran Liou <dawran6@gmail.com>
2018-12-16 20:27:56 -0800

    Align logo with textarea

--- src/markdown_editor/core.cljs
+++ src/markdown_editor/core.cljs
@@ -26,7 +26,7 @@
 
 (defn editor [app-state]
   [:div.w-100.m0.h100.dib.h-100
-   [:textarea.fl.w-50.h-100.bg-black-10.br.b--black-10.pa4.f4.code
+   [:textarea.fl.w-50.h-100.bg-black-10.br.b--black-10.pa3.pa4-l.f6.f5-m.f4-l.code
     {:onChange update-text
      :value (:text app-state)}]
    [:div.fl.w-50.h-100.ph4
