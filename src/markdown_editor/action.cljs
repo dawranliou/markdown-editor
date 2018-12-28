b83481a599a021410cb09faa171a1acd8801496e
Daw-Ran Liou <dawran6@gmail.com>
2018-12-28 14:58:08 -0800

    Implement share action

--- src/markdown_editor/action.cljs
+++ src/markdown_editor/action.cljs
@@ -1,4 +1,6 @@
-(ns markdown-editor.action)
+(ns markdown-editor.action
+  (:require [clojure.string :as string]
+            [cemerick.url :as url]))
 
 (defn download-markdown [app-state event]
   (.preventDefault event)
@@ -10,3 +12,25 @@
     (.appendChild (.-body js/document) tmp-link)
     (.click tmp-link)
     (.removeChild (.-body js/document) tmp-link)))
+
+(defn copy-text-to-clipboard [text]
+  (let [el (js/document.createElement "textarea")]
+    (set! (.-textContent el) text)
+    (.appendChild (.-body js/document) el)
+    (.select el)
+    (.execCommand js/document "copy")
+    (.removeChild (.-body js/document) el)))
+
+(defn share [app-state event]
+  (.preventDefault event)
+  (let [url (-> js/window
+                .-location
+                url/url)
+        b64-full-text (-> app-state
+                          :text
+                          string/trim
+                          js/btoa)
+        perm-link (-> url
+                      (assoc :query {:t b64-full-text})
+                      str)]
+    (copy-text-to-clipboard perm-link)))
