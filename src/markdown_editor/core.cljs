0bdde1bb19fb171640e3c44c85a78fdf88adcf41
Daw-Ran Liou <dawran6@gmail.com>
2018-12-31 16:18:07 -0800

    Migrate to marked.js for markdown translation

--- src/markdown_editor/core.cljs
+++ src/markdown_editor/core.cljs
@@ -1,6 +1,5 @@
 (ns markdown-editor.core
     (:require [reagent.core :as reagent :refer [atom]]
-              [markdown.core :refer [md->html]]
               [alandipert.storage-atom :refer [local-storage]]
               [cemerick.url :as url]
               [markdown-editor.icon :as icon]
@@ -10,26 +9,31 @@
                     (atom {:text "# Cool Markdown Editor\n\n## Features\n\n- Edit markdown in your browser\n- Save your markdown and continue later\n- Download markdown to your computer\n\n![alt screenshot](https://imgs.xkcd.com/comics/lisp_cycles.png)\n\n__TRY NOW!__"})
                     :app-state))
 
-(defn update-text [event]
-  (swap! app-state assoc :text event.target.value))
+(defn editor-component [app-state]
+  [:textarea.fl.w-50.h-100.bg-black-10.br.b--black-10.pa3.pa4-l.f6.f5-m.code
+   {:value    (:text @app-state)
+    :onChange #(swap! app-state assoc :text (-> % .-target .-value))}])
+
+(defn markdown-render-component [app-state]
+  [:div.fl.w-50.h-100.ph4
+   [:div
+    {:id "preview"
+     :dangerouslySetInnerHTML {:__html (js/marked (:text @app-state))}}]])
+
+(defn tools-component [app-state]
+  [:div.flex.items-center.fixed.bottom-2.left-2
+   [:a.black.bg-animate.hover-bg-black.items-center.pa3.ba.border-box.inline-flex.items-center.mr2
+    {:onClick (partial action/download-markdown @app-state)}
+    icon/download]
+   [:a.black.bg-animate.hover-bg-black.items-center.pa3.ba.border-box.inline-flex.items-center
+    {:onClick (partial action/share @app-state)}
+    icon/share]])
 
 (defn app [app-state]
   [:div.w-100.m0.h100.dib.h-100
-   [:textarea.fl.w-50.h-100.bg-black-10.br.b--black-10.pa3.pa4-l.f6.f5-m.code
-    {:onChange update-text
-     :value (:text @app-state)}]
-   [:div.fl.w-50.h-100.ph4
-    [:div
-     {:id "preview"
-      :dangerouslySetInnerHTML {:__html (md->html (:text @app-state))}}]]
-   [:div.flex.items-center.fixed.bottom-2.left-2
-    [:a.black.bg-animate.hover-bg-black.items-center.pa3.ba.border-box.inline-flex.items-center.mr2
-     {:onClick (partial action/download-markdown @app-state)}
-     icon/download]
-    [:a.black.bg-animate.hover-bg-black.items-center.pa3.ba.border-box.inline-flex.items-center
-     {:onClick (partial action/share @app-state)}
-     icon/share]]
-   ])
+   [editor-component app-state]
+   [markdown-render-component app-state]
+   [tools-component app-state]])
 
 (defn initialize-app-state! [app-state]
   (when-let [b64-text (-> js/window
